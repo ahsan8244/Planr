d95aae5073aa18df3ca90f563661286cf56e6a52
welvin21 <wewe2712@connect.hku.hk>
2020-11-28 17:09:19 +0800

    Change tab indent to spaces

--- types/schema.tsx
+++ types/schema.tsx
@@ -1,34 +1,34 @@
 export interface Venue {
-	name: string;
-	geolocation: {
-		longitude: {
-			x: number;
-			y: number;
-		},
-		latitude: {
-			x: number;
-			y: number;
-		}
-	}
+  name: string;
+  geolocation: {
+    longitude: {
+      x: number;
+      y: number;
+    },
+    latitude: {
+      x: number;
+      y: number;
+    }
+  }
 };
 
 export interface Course {
-	code: string;
-	name: string;
-	prereqs: Course[];
-	desc: string;
-	venue: Venue;
-	Timeslot: Date[];
+  code: string;
+  name: string;
+  prereqs: Course[];
+  desc: string;
+  venue: Venue;
+  Timeslot: Date[];
 };
 
 export interface Major {
-	name: string;
-	reqs: {
-		core: Course[],
-		elective: Course[]
-	}
+  name: string;
+  reqs: {
+    core: Course[],
+    elective: Course[]
+  }
 };
 
 export interface Schedule {
-	courses: Course[];
+  courses: Course[];
 }
